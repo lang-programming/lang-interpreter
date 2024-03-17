ddd792bf499da0ffe810538c06113f1fb3e6300f
JDDev0 <jddev0@dorneger.at>
2024-03-17 07:34:53 +0100

    Use "LangUtils.nullToLangVoid()" in "func.combW()"

--- src/main/java/at/jddev0/lang/LangPredefinedFunctions.java
+++ src/main/java/at/jddev0/lang/LangPredefinedFunctions.java
@@ -6187,7 +6187,7 @@ final class LangPredefinedFunctions {
 			DataObject retA = interpreter.callFunctionPointer(aFunc, a.getVariableName(), Arrays.asList(
 					b
 			), SCOPE_ID);
-			retA = retA == null?new DataObject().setVoid():retA;
+			retA = LangUtils.nullToLangVoid(retA);
 
 			if(retA.getType() != DataType.FUNCTION_POINTER)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_FUNC_PTR, "The value returned by a(b) must be of type " + DataType.FUNCTION_POINTER, SCOPE_ID);
