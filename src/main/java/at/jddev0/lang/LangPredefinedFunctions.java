e8bd42aae5a790420bb65a4492cc6b9fabb3864f
JDDev0 <jddev0@dorneger.at>
2024-05-14 17:03:48 +0200

    Replace "LangUtils.separateArgumentsWithArgumentSeparators()" with "LangUtils.asListWithArgumentSeparators()" in "func.combM4()"

--- src/main/java/at/jddev0/lang/LangPredefinedFunctions.java
+++ src/main/java/at/jddev0/lang/LangPredefinedFunctions.java
@@ -4715,10 +4715,8 @@ final class LangPredefinedFunctions {
 		) {
 			FunctionPointerObject aFunc = a.getFunctionPointer();
 
-			return interpreter.callFunctionPointer(aFunc, a.getVariableName(), LangUtils.separateArgumentsWithArgumentSeparators(
-					Arrays.asList(
-							a, b, c, d
-					)
+			return interpreter.callFunctionPointer(aFunc, a.getVariableName(), LangUtils.asListWithArgumentSeparators(
+					a, b, c, d
 			), SCOPE_ID);
 		}
 
