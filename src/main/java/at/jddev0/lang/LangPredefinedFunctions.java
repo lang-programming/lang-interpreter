768632c3754c2f98b1ef814fff60bb2312954c8b
JDDev0 <jddev0@dorneger.at>
2024-06-18 06:56:33 +0200

    Replace "LangUtils.separateArgumentsWithArgumentSeparators()" with "LangUtils.asListWithArgumentSeparators()" in "func.combXA()"

--- src/main/java/at/jddev0/lang/LangPredefinedFunctions.java
+++ src/main/java/at/jddev0/lang/LangPredefinedFunctions.java
@@ -5369,23 +5369,17 @@ final class LangPredefinedFunctions {
 			FunctionPointerObject aFunc = a.getFunctionPointer();
 			FunctionPointerObject bFunc = b.getFunctionPointer();
 
-			DataObject retB1 = interpreter.callFunctionPointer(bFunc, b.getVariableName(), LangUtils.separateArgumentsWithArgumentSeparators(
-					Arrays.asList(
-							c, d, c
-					)
+			DataObject retB1 = interpreter.callFunctionPointer(bFunc, b.getVariableName(), LangUtils.asListWithArgumentSeparators(
+					c, d, c
 			));
 
-			DataObject retB2 = interpreter.callFunctionPointer(bFunc, b.getVariableName(), LangUtils.separateArgumentsWithArgumentSeparators(
-					Arrays.asList(
-							d, c, d
-					)
+			DataObject retB2 = interpreter.callFunctionPointer(bFunc, b.getVariableName(), LangUtils.asListWithArgumentSeparators(
+					d, c, d
 			));
 
-			return interpreter.callFunctionPointer(aFunc, a.getVariableName(), LangUtils.separateArgumentsWithArgumentSeparators(
-					Arrays.asList(
-							LangUtils.nullToLangVoid(retB1),
-							LangUtils.nullToLangVoid(retB2)
-					)
+			return interpreter.callFunctionPointer(aFunc, a.getVariableName(), LangUtils.asListWithArgumentSeparators(
+					LangUtils.nullToLangVoid(retB1),
+					LangUtils.nullToLangVoid(retB2)
 			));
 		}
 
